567de1cf89e206efcda3b5638ba29f8fe4405289
Hwai Jiin Lee <hwaijiinlee@gmail.com>
2021-10-29 14:48:07 -0500

    updating with deliverable 2

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -21,4 +21,17 @@ FROM unique_titles as ut
 GROUP BY ut.title
 ORDER BY COUNT(ut.emp_no) DESC;
 
+--Deliverable 2
+SELECT 	DISTINCT ON (e.emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date,
+					de.from_date, de.to_date, titles.title
+INTO mentorship_eligibility
+FROM employees as e
+LEFT JOIN dept_employees as de
+ON (e.emp_no = de.emp_no)
+LEFT JOIN titles
+ON (e.emp_no = titles.emp_no)
+WHERE (de.to_date = '9999-01-01')
+	AND(e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
+
 
